6057782ec26300b3a609dd87232f15ed3dfd4e41
Edwin Eefting <edwin@datux.nl>
2018-04-27 16:57:35 +0200

    wifi

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 // #include <WiFiClient.h>
-// #include <ESP8266WebServer.h>
+#include <ESP8266WebServer.h>
 // #include <ESP8266mDNS.h>
 // #include <WiFiUdp.h>
 // #include <ArduinoOTA.h>
@@ -34,7 +34,7 @@
 // #define WIDTH 144*2
 // #define HEIGHT 9
 #define LED_COUNT WIDTH*HEIGHT
-#define FPS 30
+#define FPS 20
 
 NeoTopology <RowMajor180Layout> topo(WIDTH,HEIGHT);
 // NeoPixelBus<NEOPIXEL_CONFIG> strip(LED_COUNT);
@@ -46,40 +46,51 @@ NeoPixelBus<NeoGrbFeature,Neo800KbpsMethod> strip(LED_COUNT);
 // 32 bits elsewhere.  Try to accommodate both...
 
 #if !defined(__INT_MAX__) || (__INT_MAX__ > 0xFFFF)
- #define pgm_read_pointer(addr) ((void *)pgm_read_dword(addr))
+#define pgm_read_pointer(addr) ((void *)pgm_read_dword(addr))
 #else
- #define pgm_read_pointer(addr) ((void *)pgm_read_word(addr))
+#define pgm_read_pointer(addr) ((void *)pgm_read_word(addr))
 #endif
 
 
 
-// ESP8266WebServer server(80);
+ESP8266WebServer server(80);
 // strip_anim_c<LED_COUNT> strip_anim;
 
 
 
-// void handleRoot() {
-//     server.send(200, "text/plain", "hello from esp8266!");
-// }
-//
+void handleRoot() {
+  server.send(200, "text/plain", "hello from esp8266!");
+}
+
 
 void wifi_config()
 {
+  Serial.printf("Wifi stations connected = %d\n", WiFi.softAPgetStationNum());
+  if (WiFi.status()==WL_CONNECTED)
+  {
+    Serial.print("Wifi connected IP address: ");
+    Serial.print(WiFi.localIP());
+    Serial.print(", strength: ");
+    Serial.println(WiFi.RSSI());
+  }
+  else
+  {
     File wifi_config_fh = SPIFFS.open("/wifi.txt", "r");
     if (!wifi_config_fh) {
-        Serial.println("cannot load wifi config.");
+      Serial.println("cannot load wifi config.");
     }
     else
     {
-        String ssid=wifi_config_fh.readStringUntil('\n');
-        String password=wifi_config_fh.readStringUntil('\n');
-        wifi_config_fh.close();
+      String ssid=wifi_config_fh.readStringUntil('\n');
+      String password=wifi_config_fh.readStringUntil('\n');
+      wifi_config_fh.close();
 
-        Serial.print("Wifi connecting to: ");
-        Serial.println(ssid.c_str());
-        WiFi.setAutoReconnect(true);
-        WiFi.begin(ssid.c_str(), password.c_str());
+      Serial.print("Wifi connecting to: ");
+      Serial.println(ssid.c_str());
+      WiFi.setAutoReconnect(true);
+      WiFi.begin(ssid.c_str(), password.c_str());
     }
+  }
 }
 
 // bool smooth=false;
@@ -190,26 +201,38 @@ void wifi_config()
 // }
 
 void setup(void){
-    Serial.begin(115200);
+  Serial.begin(115200);
 
-    delay(100);
-    Serial.println();
-    Serial.println("Marquee 1.0 booting...");
+  delay(100);
+  Serial.println();
+  Serial.println("Marquee 1.0 booting...");
 
-    // Serial.println(ESP.getSketchSize());
-    // Serial.println(ESP.getFreeSketchSpace());
+  // Serial.println(ESP.getSketchSize());
+  // Serial.println(ESP.getFreeSketchSpace());
 
-    strip.Begin();
-
-
-    if (!SPIFFS.begin())
-      Serial.println("SPIFFS: error while mounting");
+  strip.Begin();
 
 
+  if (!SPIFFS.begin())
+  Serial.println("SPIFFS: error while mounting");
+
+  server.on("/", HTTP_GET, [](){
+    server.send(404, "text/plain", "FileNotFound");
+  });
 
 
-    Serial.println("boot complete");
-    Serial.println(ESP.getFreeHeap());
+  server.begin();
+
+  // WiFi.begin();
+  // WiFi.mode(WIFI_AP_STA);
+  if ( WiFi.softAP("lichtkrant1", "toolboxdinges"))
+  Serial.print("wifi ok\n");
+  else
+  Serial.print("wifi failed\n");
+
+
+  Serial.println("boot complete");
+  Serial.println(ESP.getFreeHeap());
 }
 
 
@@ -219,8 +242,9 @@ unsigned long last_check=0;
 
 void periodic_checks()
 {
-  if (last_wifi_status!=WL_CONNECTED)
-    wifi_config();
+  wifi_config();
+
+
 }
 
 
@@ -228,140 +252,141 @@ void periodic_checks()
 
 class ProgressiveScroller
 {
-  public:
-    int charnr=0;
-    int xoffset=0;
-    int space=0;
-    String text="Welkom bij TOOLBOX   ";
-    RgbColor color;
+public:
+  int charnr=0;
+  int xoffset=0;
+  int space=0;
+  String text="Welkom bij TOOLBOX   ";
+  RgbColor color;
 
 
-    void step()
+  void step()
+  {
+    //stop text as this point
+    for(int i=0;i<8; i++)
+    strip.SetPixelColor( topo.Map(3,i), RgbColor(0,0,0));
+
+
+    // if (space){
+    //   //draw empty space
+    //   for(int i=0;i<8; i++)
+    //     strip.SetPixelColor( topo.Map(WIDTH-2,i), RgbColor(0,0,0));
+    //
+    //   space--;
+    // }
+    // else
     {
-      //stop text as this point
-      for(int i=0;i<8; i++)
-        strip.SetPixelColor( topo.Map(3,i), RgbColor(0,0,0));
+      //draw one pixelline of a character
+      color=RgbColor( (charnr%6)*15, ((charnr+1)%6)*15, ((charnr+2)%6)*15) ;
+      // color=RgbColor( 0,0,255) ;
 
-
-      // if (space){
-      //   //draw empty space
-      //   for(int i=0;i<8; i++)
-      //     strip.SetPixelColor( topo.Map(WIDTH-2,i), RgbColor(0,0,0));
-      //
-      //   space--;
-      // }
-      // else
+      if (drawCharLine(text[charnr], WIDTH-1, xoffset, color))
       {
-        //draw one pixelline of a character
-        color=RgbColor( (charnr%6)*15, ((charnr+1)%6)*15, ((charnr+2)%6)*15) ;
-        // color=RgbColor( 0,0,255) ;
-
-        if (drawCharLine(text[charnr], WIDTH-1, xoffset, color))
+        //character complete
+        xoffset=0;
+        charnr=charnr+1;
+        if (charnr>=text.length())
         {
-          //character complete
-          xoffset=0;
-          charnr=charnr+1;
-          if (charnr>=text.length())
-          {
-            //scrolled through all text
-            charnr=0;
-          }
-        }
-        else
-        {
-          xoffset=xoffset+1;
+          //scrolled through all text
+          charnr=0;
         }
       }
-
-      strip.ShiftRight(1);
-    }
-
-
-    //draws one vertical fontline at the x position
-    bool drawCharLine(unsigned char c, int16_t x, int16_t xoffset, RgbColor color)
-    {
-      const uint8_t *charbase = FontData + (( c - ASCII_OFFSET)* FONT_WIDTH );
-
-
-      char pixels;
-
-      //at the end of this fonts data yet?
-      if (xoffset<FONT_WIDTH)
-        pixels=pgm_read_byte_near(charbase+xoffset);
       else
-        pixels=0;
-
-      for (int16_t y=0; y<FONT_HEIGHT; y++)
       {
-        if (pixels & (1<<(FONT_HEIGHT-y)))
-            strip.SetPixelColor( topo.Map(x,y), color);
-        else
-          strip.SetPixelColor( topo.Map(x,y), 0);
+        xoffset=xoffset+1;
       }
-
-      //letter + space complete?
-      if (xoffset>=FONT_WIDTH)
-        return true;
-      else
-        return false;
     }
 
+    strip.ShiftRight(1);
+    strip.Show();
+  }
 
-    // //obsolete for now, adafruit fonts too big for this marquee
-    // bool drawCharLine(unsigned char c, int16_t xoffset, RgbColor color)
-    // {
-    //     int x=WIDTH-2;
-    //
-    //     c -= (uint8_t)pgm_read_byte(&gfxFont->first);
-    //     GFXglyph *glyph  = &(((GFXglyph *)pgm_read_pointer(&gfxFont->glyph))[c]);
-    //     uint8_t  *bitmap = (uint8_t *)pgm_read_pointer(&gfxFont->bitmap);
-    //
-    //     uint16_t bo = pgm_read_word(&glyph->bitmapOffset);
-    //     uint8_t  w  = pgm_read_byte(&glyph->width),
-    //              h  = pgm_read_byte(&glyph->height);
-    //     int8_t   xo = pgm_read_byte(&glyph->xOffset),
-    //              yo = pgm_read_byte(&glyph->yOffset);
-    //     uint8_t  xx, yy, bits = 0, bit = 0;
-    //     int16_t  xo16 = 0, yo16 = 0;
-    //
-    //     // if(size > 1) {
-    //     //     xo16 = xo;
-    //     //     yo16 = yo;
-    //     // }
-    //
-    //     for(yy=0; yy<h; yy++) {
-    //         for(xx=0; xx<w; xx++)
-    //         {
-    //             if(!(bit++ & 7)) {
-    //                 bits = pgm_read_byte(&bitmap[bo++]);
-    //             }
-    //
-    //             if (xx==xoffset)
-    //             {
-    //               int y=yo+yy+HEIGHT-1;  //font starts at the bottom and works up
-    //
-    //               if (y>=0 && y<HEIGHT)
-    //               {
-    //                 if(bits & 0x80) {
-    //                   //pixel on
-    //                   // strip.SetPixelColor( topo.Map(x+xo+xx,y+yo+yy), color);
-    //                   strip.SetPixelColor( topo.Map(x,y), color);
-    //                 }
-    //                 else
-    //                 {
-    //                   //off
-    //                   // strip.SetPixelColor( topo.Map(x+xo+xx,y+yo+yy), RgbColor(0,0,0));
-    //                   strip.SetPixelColor( topo.Map(x,y), RgbColor(0,0,0));
-    //
-    //                 }
-    //               }
-    //             }
-    //             bits <<= 1;
-    //         }
-    //     }
-    //
-    //     return(xoffset+1>=w);
-    //   }
+
+  //draws one vertical fontline at the x position
+  bool drawCharLine(unsigned char c, int16_t x, int16_t xoffset, RgbColor color)
+  {
+    const uint8_t *charbase = FontData + (( c - ASCII_OFFSET)* FONT_WIDTH );
+
+
+    char pixels;
+
+    //at the end of this fonts data yet?
+    if (xoffset<FONT_WIDTH)
+    pixels=pgm_read_byte_near(charbase+xoffset);
+    else
+    pixels=0;
+
+    for (int16_t y=0; y<FONT_HEIGHT; y++)
+    {
+      if (pixels & (1<<(FONT_HEIGHT-y)))
+      strip.SetPixelColor( topo.Map(x,y), color);
+      else
+      strip.SetPixelColor( topo.Map(x,y), 0);
+    }
+
+    //letter + space complete?
+    if (xoffset>=FONT_WIDTH)
+    return true;
+    else
+    return false;
+  }
+
+
+  // //obsolete for now, adafruit fonts too big for this marquee
+  // bool drawCharLine(unsigned char c, int16_t xoffset, RgbColor color)
+  // {
+  //     int x=WIDTH-2;
+  //
+  //     c -= (uint8_t)pgm_read_byte(&gfxFont->first);
+  //     GFXglyph *glyph  = &(((GFXglyph *)pgm_read_pointer(&gfxFont->glyph))[c]);
+  //     uint8_t  *bitmap = (uint8_t *)pgm_read_pointer(&gfxFont->bitmap);
+  //
+  //     uint16_t bo = pgm_read_word(&glyph->bitmapOffset);
+  //     uint8_t  w  = pgm_read_byte(&glyph->width),
+  //              h  = pgm_read_byte(&glyph->height);
+  //     int8_t   xo = pgm_read_byte(&glyph->xOffset),
+  //              yo = pgm_read_byte(&glyph->yOffset);
+  //     uint8_t  xx, yy, bits = 0, bit = 0;
+  //     int16_t  xo16 = 0, yo16 = 0;
+  //
+  //     // if(size > 1) {
+  //     //     xo16 = xo;
+  //     //     yo16 = yo;
+  //     // }
+  //
+  //     for(yy=0; yy<h; yy++) {
+  //         for(xx=0; xx<w; xx++)
+  //         {
+  //             if(!(bit++ & 7)) {
+  //                 bits = pgm_read_byte(&bitmap[bo++]);
+  //             }
+  //
+  //             if (xx==xoffset)
+  //             {
+  //               int y=yo+yy+HEIGHT-1;  //font starts at the bottom and works up
+  //
+  //               if (y>=0 && y<HEIGHT)
+  //               {
+  //                 if(bits & 0x80) {
+  //                   //pixel on
+  //                   // strip.SetPixelColor( topo.Map(x+xo+xx,y+yo+yy), color);
+  //                   strip.SetPixelColor( topo.Map(x,y), color);
+  //                 }
+  //                 else
+  //                 {
+  //                   //off
+  //                   // strip.SetPixelColor( topo.Map(x+xo+xx,y+yo+yy), RgbColor(0,0,0));
+  //                   strip.SetPixelColor( topo.Map(x,y), RgbColor(0,0,0));
+  //
+  //                 }
+  //               }
+  //             }
+  //             bits <<= 1;
+  //         }
+  //     }
+  //
+  //     return(xoffset+1>=w);
+  //   }
 };
 
 
@@ -378,71 +403,55 @@ unsigned long idle_us=0;
 
 
 void loop(void){
-    // Serial.println("lup");
-    //server.handleClient();
+  // Serial.println("lup");
+  server.handleClient();
 
 
-    if (WiFi.status() != last_wifi_status)
-    {
-        last_wifi_status=WiFi.status();
-        if (last_wifi_status==WL_CONNECTED)
-        {
-            Serial.print("Wifi connected IP address: ");
-            Serial.print(WiFi.localIP());
-            Serial.print(", strength: ");
-            Serial.println(WiFi.RSSI());
-        }
-        else
-        {
-            Serial.print("Wifi disconnected, status:");
-            Serial.println(last_wifi_status);
-        }
-
-    }
 
 
-    //ArduinoOTA.handle();
+  //ArduinoOTA.handle();
 
-    //do periodic checks ever 10s
-    if ((millis()-last_check)>1000)
-    {
-      Serial.printf("Load: %d\n", 100- ((idle_us/10)/(millis()-last_check) ));
-      idle_us=0;
+  //do periodic checks ever 10s
+  if ((millis()-last_check)>1000)
+  {
+    Serial.printf("Load: %d\n", 100- ((idle_us/10)/(millis()-last_check) ));
+    idle_us=0;
 
-      periodic_checks();
-      last_check=millis();
-    }
+    periodic_checks();
+    last_check=millis();
+  }
 
 
-    //profiling
-    // Serial.print("microseconds used: ");
-    // Serial.println(micros()-last_micros);
-    // with the last tests i did it was usually lower than 2000uS (for both strips)
+  //profiling
+  // Serial.print("microseconds used: ");
+  // Serial.println(micros()-last_micros);
+  // with the last tests i did it was usually lower than 2000uS (for both strips)
 
 
 
 
 
-    // strip.ClearTo(RgbColor(0,0,0));
-    scroller.step();
-    while(!strip.CanShow()) yield();
-    strip.Show();
-    // strip.RotateLeft(1);
+  // strip.ClearTo(RgbColor(0,0,0));
+  // while(!strip.CanShow()) yield();
+  //strip.Show();
+  // strip.RotateLeft(1);
 
-    //cap at 60 fps
-    // while (micros()-last_micros < 16666);
-    int count=0;
+  //cap at 60 fps
+  // while (micros()-last_micros < 16666);
+  int count=0;
 
-    unsigned long delta=micros()-last_micros;
-    // Serial.println(delta);
-    if (delta < (1000000/FPS))
-    {
-      unsigned long time_left= (1000000/FPS)  - ( delta);
-      idle_us=idle_us+time_left;
-      delayMicroseconds(time_left);
-    }
-//    while (micros()-last_micros < 1500*0.03*2) count++;
-    // Serial.println(count);
-    last_micros=micros();
+  unsigned long delta=micros()-last_micros;
+  // Serial.println(delta);
+  if (delta < (1000000/FPS))
+  {
+    unsigned long time_left= (1000000/FPS)  - ( delta);
+    idle_us=idle_us+time_left;
+    // delayMicroseconds(time_left);
+    delay(time_left/1000);
+  }
+  //    while (micros()-last_micros < 1500*0.03*2) count++;
+  // Serial.println(count);
+  last_micros=micros();
+  scroller.step();
 
 }
